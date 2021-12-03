bc5e2d85956db3c5aa54cd380ccb3608b625cac1
CarlinGebyte <91474067+CarlinGebyte@users.noreply.github.com>
2021-12-03 18:04:08 -0500

    First commit

---
+++ src/main/java/com/usa/bicipool/BicipoolApplication.java
@@ -0,0 +1,13 @@
+package com.usa.bicipool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicipoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicipoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usa/bicipool/BicipoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usa.bicipool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicipoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
